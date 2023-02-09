ed880c51089311275a7f3b3933640ca87b09ad98
dmstn0526 <dmstn_0526@naver.com>
2023-02-09 14:52:32 +0900

    4. 삭제기능

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -3,7 +3,7 @@
         <header><h1>Vue Fire todo1</h1></header>
         <main>
         <div class="todos">
-            <div class="write">
+            <div class="write" v-if="writeState==='add'"> <!--등록-->
             <input
                 ref="writeArea"
                 type="text"
@@ -11,6 +11,15 @@
                 @keyup.enter="addItem"/>
             <button class="btn add" @click="addItem">Add</button>
             </div>
+            <div class="write" v-else> <!--수정-->
+            <input
+                ref="writeArea"
+                type="text"
+                v-model="editItemText"
+                @keyup.enter="editSave"/>
+            <button class="btn add" @click="editSave">Save</button>
+            </div>
+
             <ul class="list">
             <li v-for="(todo,i) in todos" :key="todo.i">
                 <i 
@@ -18,8 +27,8 @@
                 :class="[todo.state === 'yet'? 'far' : 'fas','fa-check-square']"></i>
                 <span>{{todo.text}}
                 <b>
-                    <a href="">Edit</a>
-                    <a href="">Del</a>
+                    <a href="" @click.prevent="editShow(i)">Edit</a>
+                    <a href="" @click.prevent="delItem(i)">Del</a>
                 </b>
                 </span>
             </li>
@@ -33,7 +42,10 @@
 export default {
     data() {
         return {
+            writeState: 'add',
             addItemText: '',
+            editItemText: '',
+            crrEditItem: '',
             todos:[
                 {text: '공부하기', state: 'yet'},
                 {text: '운동하기', state: 'done'}, //done일때는 fas / done일때는 far
@@ -53,7 +65,19 @@ export default {
             } else if (this.todos[index].state === 'done'){
                 this.todos[index].state = 'yet'
             }
-        }
+        },
+        editShow(index) {
+            this.crrEditItem = index;
+            this.writeState = 'edit';
+            this.editItemText = this.todos[index].text;
+        },
+        editSave() {
+            this.todos[this.crrEditItem].text = this.editItemText;
+            this.writeState = 'add';
+        },
+        delItem(index) {
+            this.todos.splice(index, 1);
+        },
     },
     mounted() {
         this.$refs.writeArea.focus();
